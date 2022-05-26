c98b9e9fd5defa8138e143cc5dc2a64a54d64112
Ole Herman Schumacher Elgesem <ole@northern.tech>
2022-05-26 17:05:24 +0200

    Updated code after webinar
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- alerting_system/alerting_system.py
+++ alerting_system/alerting_system.py
@@ -3,72 +3,41 @@ import os
 import requests
 from cfengine import PromiseModule, ValidationError, Result
 
-
-def sh(cmd):
-    # TODO: Using something like subprocess is more flexible than os.system
-    output = os.system(cmd)
-    return (0, output)
-
-
 class AlertingSystemPromiseTypeModule(PromiseModule):
-    def log(self, component, message):
-        """Log a message to cf-agent / file / HTTP only
-
-        Only log if not already present in log file.
-        Returns true if we did log something, false otherwise.
-        """
+    def log(self, message, level="info"):
         log_file = "/var/cfengine/alerting_log"
-
-        # Create file if it doesn't exist: (TODO: don't use shell for this)
         if not os.path.exists(log_file):
-            sh("touch " + log_file)
+            os.system("touch " + log_file)
+        if not os.path.exists(log_file):
+            self.log_error("Failed to create log file")
+            return Result.NOT_KEPT
 
-        # The full message we want to log:
-        message = f"{component} : {message}"
-
-        # Check if it's already logged in the log file:
         with open(log_file, "r") as f:
             if message in f.read():
-                # Log message already there, do nothing:
-                return False
+                return Result.REPAIRED
 
-        # Not logged, let's do it:
         with open(log_file, "a") as f:
-            # Log to file:
             f.write(message + "\n")
-            # And to HTTP API:
-            data = str({"message": message, "component": component})
-            requests.post(
-                "http://127.0.0.1:5000/",
-                data=data,
-            )
-            # And to the agent / terminal:
-            self.log_info(message)
-
-            # For an alternative to requests which is not another dependency,
-            # see urllib: https://docs.python.org/3/library/urllib.request.html
-
-            # We could also use curl:
-            # sh(f"curl -X POST --data '{data}' 'http://127.0.0.1:5000/'")
-        return True
+            # requests.post("http://127.0.0.1:5000/", data = str({"message": message}))
+            self._log(level, message)
+        return Result.KEPT
 
     def validate_promise(self, promiser, attributes):
         if not promiser:
             raise ValidationError(f"Promiser must be non-empty")
-        if not "message" in attributes:
-            raise ValidationError(f"Missing message attribute")
+        if "component" not in attributes:
+            raise ValidationError("Missing required attribute 'component'")
+
+        if "severity" in attributes and attributes["severity"] not in ("info", "error"):
+            raise ValidationError("Invalid severity: " + attributes["severity"])
 
     def evaluate_promise(self, promiser, attributes):
-        component = promiser
-        message = attributes["message"]
+        message = promiser
+        component = attributes["component"]
+        severity = attributes.get("severity", "unknown")
+        message = f"{message} - {component} - {severity}"
 
-        # Important: the module should check whether changes are necessary
-        #            and return KEPT if everything was already as desired
-        if self.log(component, message):
-            # The log method did log something
-            return Result.REPAIRED
-        # Already logged, no changes made this time
-        return Result.KEPT
+        return self.log(message, "info" if severity == "unknown" else severity)
 
 
 if __name__ == "__main__":
